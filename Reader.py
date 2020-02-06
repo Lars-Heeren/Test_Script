5ab567ec8a83c6d4ecc26bc3bfbb6c079a210351
Lars <La.Heeren@student.avans.nl>
2020-02-06 16:16:18 +0100

    fix python hopefully test

--- Reader.py
+++ Reader.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 CHROME_DRIVER = "\\chromedriver.exe"
 CHROME_PATH = os.path.dirname(os.path.abspath(__file__)) + "\\chrome"
-APPLICATION_ADDRESS_STRING = "http://localhost/"
+APPLICATION_ADDRESS_STRING = "http://localhost/phpweek2/index.html"
 
 
 class Reader:
@@ -15,8 +15,8 @@ class Reader:
         options.add_argument('user-data-dir=' + CHROME_PATH)
         print(os.path.dirname(os.path.abspath(__file__)))
 
-        driver = webdriver.Chrome(executable_path=CHROME_PATH + CHROME_DRIVER, chrome_options=options)
-        driver.get(APPLICATION_ADDRESS_STRING)
+        self.driver = webdriver.Chrome(executable_path=CHROME_PATH + CHROME_DRIVER, options=options)
+        self.driver.get(APPLICATION_ADDRESS_STRING)
 
         try:
             self.test_application()
@@ -24,10 +24,13 @@ class Reader:
             print("test setting failed please check setup")
 
         input("press Enter to quit")
-        driver.quit()
+        self.driver.quit()
 
     def test_application(self):
-        # add testing code here
+        self.driver.find_element_by_name("firstname").send_keys("Dennis")
+        self.driver.find_element_by_name("lastname").send_keys("sjaak")
+        self.driver.find_element_by_name("bijbel").send_keys("holy godddddddddd")
+        self.driver.find_element_by_css_selector("input[type='submit']").click()
         pass
 
 
