90911c9acbce332a780cf45cead65d8c195bb42b
Melvin Hsieh <mhsieh@avans.nl>
2020-03-08 23:26:58 +0100

    algemene refactor
    Comments aangepast, onnodige prints weggehaald, object structuur aangepast, NewProjectsTests.py tests aangepast

--- Reader.py
+++ Reader.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from test.CarrouselTests import CarrouselTests
+from test.MapTests import MapTests
 from test.NavigationBarTests import NavigationBarTests
 from test.NewProjectsTests import NewProjectsTests
 
@@ -15,47 +16,21 @@ APPLICATION_ADDRESS_STRING = " http://127.0.0.1:8000"
 class Reader:
     def __init__(self):
         options = Options()
-        # options.add_argument('user-data-dir=' + CHROME_PATH)
-        print(os.path.dirname(os.path.abspath(__file__)))
-
         self.driver = webdriver.Chrome(executable_path=CHROME_PATH + CHROME_DRIVER, options=options)
         self.driver.get(APPLICATION_ADDRESS_STRING)
-
-        try:
-            self.test_application()
-        except IndexError:
-            print("test setting failed please check setup")
-            self.driver.quit()
-
-        input("press Enter to quit")
+        self.run_tests()
         self.driver.quit()
-        self.driver.close()
 
-    def test_application(self):
+    def run_tests(self):
         tests = []
 
         tests.append(NavigationBarTests(self.driver))
         tests.append(CarrouselTests(self.driver))
         tests.append(NewProjectsTests(self.driver))
+        tests.append(MapTests(self.driver))
 
         for test in tests:
-            test.run_tests()
-
-        # img1 = self.driver.find_element_by_css_selector(".primary_project img")
-
-        # carrousel = self.driver.find_element_by_class_name("primary_project")
-        # hover = ActionChains(self.driver).move_to_element(carrousel)
-        # hover.perform()
-
-    # button = self.driver.find_element_by_id("next")
-    # click = ActionChains(self.driver).click(button)
-    # click.perform()
-
-    # img2 = self.driver.find_element_by_css_selector(".primary_project img")
-
-    # print(img1 != img2)
-
-    # pass
+            test.run()
 
 
 Reader()
--- test/NewProjectsTests.py
+++ test/NewProjectsTests.py
@@ -1,83 +1,47 @@
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 from test.TestClass import TestClass
 
-class NewProjectsTests(TestClass):
 
+class NewProjectsTests(TestClass):
+    # Deze tests moeten nog uitgebreider, met dat je werkelijk ook op de pagina terecht komt
     def __init__(self, driver, name="NewProjects"):
         super().__init__(driver, name)
 
+    def run(self):
+        self.tests["test_newprojects1_link"] = self.test_newprojects1_link()
+        self.tests["test_newprojects2_link"] = self.test_newprojects2_link()
+        self.tests["test_newprojects3_link"] = self.test_newprojects3_link()
+        self.tests["test_newprojects4_link"] = self.test_newprojects4_link()
+        super().run()
 
-    def run_tests(self):
-        self.tests["test_NewProjects1_Link"] = self.test_NewProjects1_Link()
-        self.tests["test_NewProjects2_Link"] = self.test_NewProjects2_Link()
-        self.tests["test_NewProjects3_Link"] = self.test_NewProjects3_Link()
-        self.tests["test_NewProjects4_Link"] = self.test_NewProjects4_Link()
-        self.print_result()
+    def test_newprojects1_link(self):
+        self.driver.get("http://localhost:8000/")
+        current_url = self.driver.current_url
+        element = self.driver.find_elements_by_css_selector(".new_project_container a")[0]
+        element.click()
+        return current_url != self.driver.current_url
 
-    def print_result(self):
-        super().print_result()
+    def test_newprojects2_link(self):
+        self.driver.get("http://localhost:8000/")
+        current_url = self.driver.current_url
+        element = self.driver.find_elements_by_css_selector(".new_project_container a")[1]
+        element.click()
+        return current_url != self.driver.current_url
 
+    def test_newprojects3_link(self):
+        self.driver.get("http://localhost:8000/")
+        current_url = self.driver.current_url
+        element = self.driver.find_elements_by_css_selector(".new_project_container a")[2]
+        element.click()
+        return current_url != self.driver.current_url
 
-    def print_failure(self):
-        super().print_failure()
-        print(self.result)
-
-    def test_NewProjects1_Link(self):
-        result = []
-        expectedurl = "http://localhost:8000/projects/display"
-        # door iedere link gaan en kijken of er iets veranderd qua url
-        elems = []
-        elems = self.driver.find_elements_by_css_selector(".new_project_container a")
-
-        elems[0].click()
-        url = self.driver.current_url
-        url = url.split("/")[0]
-        result.append(url == expectedurl)
-        self.driver.back()
-        return result
-
-    def test_NewProjects2_Link(self):
-        result = []
-        expectedurl = "http://localhost:8000/projects/display"
-        # door iedere link gaan en kijken of er iets veranderd qua url
-        elems = []
-        elems = self.driver.find_elements_by_css_selector(".new_project_container a")
-
-        elems[1].click()
-        url = self.driver.current_url
-        url = url.split("/")[0]
-        result.append(url == expectedurl)
-        self.driver.back()
-        return result
-
-    def test_NewProjects3_Link(self):
-        result = []
-        expectedurl = "http://localhost:8000/projects/display"
-        # door iedere link gaan en kijken of er iets veranderd qua url
-        elems = []
-        elems = self.driver.find_elements_by_css_selector(".new_project_container a")
-
-        elems[2].click()
-        url = self.driver.current_url
-        url = url.split("/")[0]
-        result.append(url == expectedurl)
-        self.driver.back()
-        return result
-
-    def test_NewProjects4_Link(self):
-        result = []
-        expectedurl = "http://localhost:8000/projects/display"
-        # door iedere link gaan en kijken of er iets veranderd qua url
-        elems = []
-        elems = self.driver.find_elements_by_css_selector(".new_project_container a")
-
-        elems[3].click()
-        url = self.driver.current_url
-        url = url.split("/")[0]
-        result.append(url == expectedurl)
-        self.driver.back()
-        return result
+    def test_newprojects4_link(self):
+        self.driver.get("http://localhost:8000/")
+        current_url = self.driver.current_url
+        element = self.driver.find_elements_by_css_selector(".new_project_container a")[3]
+        element.click()
+        return current_url != self.driver.current_url
