d4b6156efa2de5a6eb55f4cb794aa553ba02ac6d
Jeroen <Jeroen.gelderland@hotmail.com>
2020-05-14 12:13:27 +0200

    no message

--- handmatig/AutoFill.py
+++ handmatig/AutoFill.py
@@ -1,25 +1,40 @@
-from datetime import time
+import os
+import time
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common import exceptions
+
+CHROME_DRIVER = "\\chromedriver.exe"
+CHROME_PATH = "C:\\Users\\jeroe\\OneDrive\\Documenten\\Test_Script\\chrome"
+options = Options()
 
 
 class AutoFill:
 
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path=CHROME_PATH + CHROME_DRIVER, options=options)
         self.login()
 
-
     def login(self):
-        self.driver.get("http://localhost:8000/login")
-        link = self.driver.find_element_by_css_selector('ul.login_search li a')
-        link.click()
-        time.sleep(3)
-        emailInput = self.driver.find_element_by_id('email')
-        emailInput.send_keys('pascal@gmail.com')
+        is_loading = True
 
-        passwordInput = self.driver.find_element_by_id('password')
-        passwordInput.send_keys('weerbaarBrabant')
+        while is_loading:
+            try:
+                self.driver.get("http://localhost:8000/login")
 
-        submit = self.driver.find_element_by_css_selector('button')
-        submit.click()
-        time.sleep(3)
+                link = self.driver.find_element_by_css_selector('ul.login_search li a')
+                link.click()
+                emailInput = self.driver.find_element_by_id('email')
+                emailInput.send_keys('pascal@gmail.com')
+
+                passwordInput = self.driver.find_element_by_id('password')
+                passwordInput.send_keys('weerbaarBrabant')
+
+                submit = self.driver.find_element_by_css_selector('button')
+                submit.click()
+                is_loading = False
+            except selenium.common.exceptions.NoSuchElementException:
+                is_loading = True
 
